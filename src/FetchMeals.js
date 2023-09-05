d31c51ec4d69dd837aa64040e92b1dc57b622d57
Kristina <111386621+Kristina136@users.noreply.github.com>
2023-09-05 18:44:02 +0200

    version 1.2

--- src/FetchMeals.js
+++ src/FetchMeals.js
@@ -12,7 +12,7 @@ const getAllMeal = (setMeal)=> {
 
 //say to add new Meal and display what was before
 const addMeal = (title, ingredients, setIngredients, setTitle, setMeal, dayOfWeek, setDayOfWeek)=>{
-    axios.post(`https://meal-plan-utc0.onrender.com`, {title, ingredients, dayOfWeek})
+    axios.post(`https://meal-plan-utc0.onrender.com/saveMeals`, {title, ingredients, dayOfWeek})
     .then(()=>{
  //after push ADD empty inputs and update all list 
         setTitle("")
@@ -20,11 +20,10 @@ const addMeal = (title, ingredients, setIngredients, setTitle, setMeal, dayOfWee
         setDayOfWeek("")
         getAllMeal(setMeal)
     })
-     .catch((err)=>{console.log(err)})
     }
 
 const editMeal = (mealId, title, ingredients, setIngredients, setMeal, setTitle, setEditing1, dayOfWeek, setDayOfWeek)=>{
-    axios.post(`https://meal-plan-utc0.onrender.com`, {_id: mealId, title, ingredients, dayOfWeek})
+    axios.post(`https://meal-plan-utc0.onrender.com/deleteMeal`, {_id: mealId, title, ingredients, dayOfWeek})
         .then(()=>{
             setTitle("")
             setIngredients("")
@@ -36,7 +35,7 @@ const editMeal = (mealId, title, ingredients, setIngredients, setMeal, setTitle,
 
 
 const deleteMeal = (_id, setMeal)=>{
-    axios.post(`https://meal-plan-utc0.onrender.com`, {_id})
+    axios.post(`https://meal-plan-utc0.onrender.com/editMeal`, {_id})
         .then(()=>{
             getAllMeal(setMeal)
         })
