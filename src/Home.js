85f8b61aa7e4b63752f0468021f5ce538c10ee75
Kristina <111386621+Kristina136@users.noreply.github.com>
2023-09-14 14:55:27 +0200

    version 1.4

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
 import { MyMeal } from "./MyMeal";
 import { getAllMeal, addMeal, editMeal, deleteMeal } from "./FetchMeals";
-
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 
 function Home() {
   //create state with data from API
